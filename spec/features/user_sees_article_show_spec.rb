1dc2636a77f8a39315cc58ff80b33a6c6794afbd
amj133 <amj@vt.edu>
2018-02-02 14:52:21 -0700

    user sees show page feature test written

---
+++ spec/features/user_sees_article_show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "user sees one article" do
+  describe "they link from article index" do
+    it "displays individual article information" do
+      article = Article.create!(title: "Muffins", body: "Why muffins suck")
+
+      visit "/articles"
+
+      click_link(article.title)
+
+      expect(current_path).to eq('/Muffins/1')
+      expect(page).to have_content(article.title)
+      expect(page).to have_content(article.body)
+    end
+  end
+end
