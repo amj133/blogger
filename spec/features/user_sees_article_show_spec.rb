8eb25a28187a11b97bcede5caa24099036b44635
amj133 <amj@vt.edu>
2018-02-02 17:46:38 -0700

    show page and feature test working

--- spec/features/user_sees_article_show_spec.rb
+++ spec/features/user_sees_article_show_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 describe "user sees one article" do
   describe "they link from article index" do
     it "displays individual article information" do
-      article = Article.create!(title: "Muffins", body: "Why muffins suck")
+      article = Article.create(title: "Muffins", body: "Why muffins suck")
 
       visit "/articles"
 
       click_link(article.title)
 
-      expect(current_path).to eq('/Muffins/1')
+      expect(current_path).to eq('/articles/1')
       expect(page).to have_content(article.title)
       expect(page).to have_content(article.body)
     end
